b1150c761bb918b0e7abc5caf2415bb3e1bc3f54
marshmallie7 <ppreston755@gmail.com>
2024-11-11 12:15:43 -0800

    GetValueOrDefault

---
+++ src/Franzo.Essentials.NetStandard/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+namespace Franzo.Essentials;
+
+public static class DictionaryExtensions
+{
+    public static TValue GetValueOrDefault<TKey, TValue>(
+        this IDictionary<TKey, TValue> self,
+        TKey key,
+        Func<TValue> defaultValueFactory) where TKey : notnull
+    {
+        if (!self.TryGetValue(key, out var value))
+        {
+            value = defaultValueFactory.Invoke();
+            self[key] = value;
+        }
+
+        return value;
+    }
+}
