ec2b4dc0889118cdda3eedf17620a32f557f6939
marshmallie7 <ppreston755@gmail.com>
2024-12-12 00:42:31 -0800

    fix Number etc.

--- src/Franzo.Essentials.InterfaceInheritance.Generator/AnalysisData.cs
+++ src/Franzo.Essentials.InterfaceInheritance.Generator/AnalysisData.cs
@@ -4,9 +4,9 @@ namespace Franzo.Essentials.InterfaceInheritance.Generator;
 
 internal class AnalysisData
 {
-    public readonly List<InternalTypeSymbol> RootTypesToEmit = new();
-    public readonly List<InternalTypeSymbol> TypesCreatedDuringTypeInitializationPhase1 = new();
-    public readonly List<InternalTypeSymbol> TypesCreatedDuringTypeInitializationPhase2 = new();
+    public readonly List<InternalTypeSymbol> RootTypesToEmit = [];
+    public readonly List<InternalTypeSymbol> TypesCreatedDuringTypeInitializationPhase1 = [];
+    public readonly List<InternalTypeSymbol> TypesCreatedDuringTypeInitializationPhase2 = [];
     public readonly Dictionary<INamedTypeSymbol, InternalTypeSymbol> TypesByRoslynType = new(
         SymbolEqualityComparer.Default);
 
