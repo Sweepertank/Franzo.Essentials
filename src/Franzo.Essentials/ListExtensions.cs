1ce641c8d85fffd5d276e9e8b0d6ab0cc0d5be63
marshmallie7 <ppreston755@gmail.com>
2024-12-29 13:52:47 -0800

    fix BinaryInsert

--- src/Franzo.Essentials/ListExtensions.cs
+++ src/Franzo.Essentials/ListExtensions.cs
@@ -5,7 +5,10 @@ public static class ListExtensions
     public static void BinaryInsert<T>(this List<T> self, T item, IComparer<T>? comparer = null)
     {
         var index = self.BinarySearch(item, comparer);
-        index = ~index;
+        if (index < 0)
+        {
+            index = ~index;
+        }
         self.Insert(index, item);
     }
 }
