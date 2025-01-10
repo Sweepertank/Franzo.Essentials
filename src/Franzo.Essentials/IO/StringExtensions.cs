8dc5a5c981f94ca9cfeb874fdc81ad11de8fa625
marshmallie7 <ppreston755@gmail.com>
2025-01-10 01:46:29 -0800

    seemingly fix file synchronization

---
+++ src/Franzo.Essentials/IO/StringExtensions.cs
@@ -0,0 +1,22 @@
+namespace Franzo.Essentials;
+
+public static class StringExtensions
+{
+    // https://stackoverflow.com/questions/26102809/c-sharp-check-if-a-file-path-contains-a-specific-directory
+    // @Robustness: case sensitivity should be platform dependent, doesn't look at . or ..,
+    // probably a million other things
+    public static bool FileSystemPathIsRootedIn(this string self, string directoryPath)
+    {
+        if (!directoryPath.EndsWith(Path.DirectorySeparatorChar))
+        {
+            directoryPath = $"{directoryPath}{Path.DirectorySeparatorChar}";
+        }
+
+        if (self.StartsWith(Path.DirectorySeparatorChar))
+        {
+            self = self[1..];
+        }
+
+        return self.StartsWith(directoryPath);
+    }
+}
