7c8f98dd58fd451276d21ec09969453378483251
marshmallie7 <ppreston755@gmail.com>
2025-01-19 11:51:29 -0800

    shoobibbity

--- src/Franzo.Essentials/EnumerableExtensions.cs
+++ src/Franzo.Essentials/EnumerableExtensions.cs
@@ -2,13 +2,14 @@ namespace Franzo.Essentials;
 
 public static class EnumerableExtensions
 {
-    public static bool NullableSequenceEquals<T>(
+    public static bool NullableSequenceEqual<T>(
         this IEnumerable<T>? self,
-        IEnumerable<T>? other)
+        IEnumerable<T>? other,
+        IEqualityComparer<T>? equalityComparer = null)
     {
         if (self is null) return other is null;
         if (other is null) return self is null;
 
-        return self.SequenceEqual(other);
+        return self.SequenceEqual(other, equalityComparer);
     }
 }
--- src/Franzo.Essentials/ReadOnlyDictionaryExtensions.cs
+++ src/Franzo.Essentials/ReadOnlyDictionaryExtensions.cs
@@ -4,8 +4,20 @@ public static class ReadOnlyDictionaryExtensions
 {
     public static bool AreValueEqual<TKey, TValue>(
         IReadOnlyDictionary<TKey, TValue> a,
-        IReadOnlyDictionary<TKey, TValue> b) where TKey : notnull
+        IReadOnlyDictionary<TKey, TValue> b,
+        IEqualityComparer<TValue>? valueComparer = null) where TKey : notnull
     {
-        return new DictionaryValueEqualComparer<TKey, TValue>().Equals(a, b);
+        return new DictionaryValueEqualComparer<TKey, TValue>(valueComparer).Equals(a, b);
+    }
+
+    public static bool IsValueEqualTo<TKey, TValue>(
+        this IReadOnlyDictionary<TKey, TValue>? self,
+        IReadOnlyDictionary<TKey, TValue>? other,
+        IEqualityComparer<TValue>? valueComparer = null) where TKey : notnull
+    {
+        if (self is null) return other is null;
+        if (other is null) return self is null;
+
+        return AreValueEqual(self, other, valueComparer);
     }
 }
