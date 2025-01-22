3a680f779d32ef5cd83cf1a569d8d832fd7f01bb
marshmallie7 <ppreston755@gmail.com>
2025-01-22 06:00:58 -0800

    transformed type refactor mostly done (untested)

--- src/Franzo.Essentials/EnumExtensions.cs
+++ src/Franzo.Essentials/EnumExtensions.cs
@@ -5,6 +5,19 @@ public static class EnumExtensions
     /*public static Enum UnsetFlag(this Enum self, Enum flag)
     {
     }*/
+    public static T WithFlags<T>(this T self, T flags) where T : struct, Enum
+    {
+        var intSelf = (int)(object)self;
+        var intFlags = (int)(object)flags;
+        return (T)(object)(intSelf | intFlags);
+    }
+
+    public static T WithoutFlags<T>(this T self, T flag) where T : struct, Enum
+    {
+        var intSelf = (int)(object)self;
+        var intFlags = (int)(object)flag;
+        return (T)(object)(intSelf & ~intFlags);
+    }
 
     public static IEnumerable<T> Flags<T>(this T self) where T : struct, Enum
     {
