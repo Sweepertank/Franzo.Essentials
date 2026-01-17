a9979d6cf3d0e1ecd2548d70c429938143e4f11a
marshmallie7 <epreston0721@gmail.com>
2026-01-16 22:52:46 -0800

    ObjectExtensions.DefaultEquals

--- src/Franzo.Essentials.NetStandard/ObjectExtensions.cs
+++ src/Franzo.Essentials.NetStandard/ObjectExtensions.cs
@@ -4,6 +4,11 @@ namespace Franzo.Essentials;
 
 public static class ObjectExtensions
 {
+    public static bool DefaultEquals<T>(this T self, T other)
+    {
+        return EqualityComparer<T>.Default.Equals(self, other);
+    }
+
     public static string Repeat(this object self, int count)
     {
         var sb = new StringBuilder();
